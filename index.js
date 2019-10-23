3715eb27b44e87bf3d0060615ab02c6e8a799313
Robert Beckett <robert.beckett@onion.red>
2019-10-23 23:15:13 +0000

    Added log and complete task functions.

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ const newTask = function(title, description) {
   return task;
 };
 
+const logTaskState = function(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+};
+
+const completeTask = function(task) {
+  task.complete = true;
+};
+
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundy", "😨");
 const tasks = [task1, task2];
+
+logTaskState(task1);
+completeTask(task1);
+logTaskState(task1);
 
 console.log(tasks);
